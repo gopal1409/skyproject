3c322c261cec79984073a8c8278a7d300a2e54ed
gopal <gopal1409@gmail.com>
2026-03-04 21:02:32 +0530

    sql injection valnurability resolved

--- backend/src/main/java/com/lnd/controller/UserController.java
+++ backend/src/main/java/com/lnd/controller/UserController.java
@@ -2,18 +2,23 @@ package com.lnd.controller;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @GetMapping("/search")
-public String searchUser(@RequestParam String username) {
+    public List<Map<String, Object>> searchUser(@RequestParam String username) {
 
-    String query = "SELECT * FROM users WHERE username = ?";
+        String query = "SELECT * FROM users WHERE username = ?";
 
-    return jdbcTemplate.queryForObject(query, new Object[]{username}, String.class);
-}
+        return jdbcTemplate.queryForList(query, username);
+    }
 }
